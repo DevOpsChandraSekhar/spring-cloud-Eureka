f5d923c366e93265b35902ddd0e099a7aaff59a1
chandrasekhar <csrcg19@gmail.com>
2020-12-28 13:47:09 +0530

    spring cloud discovery

---
+++ EnableEureka-Server/src/main/java/com/csrk/EnableEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.csrk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EnableEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnableEurekaServerApplication.class, args);
+	}
+
+}
---
+++ EnableEureka-Server/src/test/java/com/csrk/EnableEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csrk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnableEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
